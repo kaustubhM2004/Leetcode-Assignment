b93942e272eaa916f34c84570f2fda186aee4d59
kaustubhM2004 <kaustubhmishra2004@gmail.com>
2026-04-26 21:38:44 +0530

    Added Best Time to Buy and Sell Stock II solution

---
+++ BestTimeToBuySellStockII.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+}
+
+public class BestTimeToBuySellStockII {
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+
+        int ans = new Solution().maxProfit(prices);
+
+        System.out.println(ans);
+    }
+}
