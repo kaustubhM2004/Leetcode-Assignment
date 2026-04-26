851ca8872806e5e5cf21246cb58eb1528c747171
kaustubhM2004 <kaustubhmishra2004@gmail.com>
2026-04-26 21:30:03 +0530

    Added Merge Sorted Array solution

---
+++ MergeSortedArray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+}
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+
+        new Solution().merge(nums1, m, nums2, n);
+
+        for (int x : nums1) {
+            System.out.print(x + " ");
+        }
+    }
+}
